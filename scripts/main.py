15d233bbf3b5b392b729d276d55f399de33377ac
Gustavo Nery <nery.gustavob@outlook.com>
2024-09-25 18:20:25 -0300

    matplotlib system and small changes

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,9 @@ import geopandas as gpd
 import os
 import time
 import pickle
+import matplotlib.pyplot as plt
+
+tempoInicio = time.time()
 
 def cache_shapefile(shapefile_path, cache_path):
     if os.path.exists(cache_path):
@@ -14,20 +17,6 @@ def cache_shapefile(shapefile_path, cache_path):
         with open(cache_path, 'wb') as f:
             pickle.dump(gdf, f)
     return gdf
-
-tempoInicio = time.time()
-shapefile_path = os.environ.get("pathShapefile")
-cache_path = os.environ.get("pathCache")
-
-anoProcurado = int(input("digite o ano: ")) 
-
-if not os.path.exists(shapefile_path):
-    print(f"Arquivo não encontrado: {shapefile_path}")
-else:
-    gdf = cache_shapefile(shapefile_path, cache_path)   
-
-gdf_sorted = gdf.sort_values(by="year", ascending=True)
-dates = gdf_sorted["year"].tolist()
 
 def pesquisaBinaria(lista, id):
     baixo = 0
@@ -45,7 +34,19 @@ def pesquisaBinaria(lista, id):
         else:
             baixo = meio + 1
     return primeiroDado
-        
+
+
+shapefile_path = os.environ.get("pathShapefile")
+cache_path = os.environ.get("pathCache")
+anoProcurado = int(input("digite o ano: ")) 
+
+if not os.path.exists(shapefile_path):
+    print(f"Arquivo não encontrado: {shapefile_path}")
+else:
+    gdf = cache_shapefile(shapefile_path, cache_path)   
+
+gdf_sorted = gdf.sort_values(by="year", ascending=True)
+dates = gdf_sorted["year"].tolist()
 anoEncontrado = pesquisaBinaria(dates, anoProcurado)
 
 if anoEncontrado is not None:
@@ -58,6 +59,27 @@ if anoEncontrado is not None:
     print("resultado encontrado: \n",resultadoDado)
 else:
     print("data não foi encontrada")
+
+
+if resultadoDado.crs is None:
+    resultadoDado = resultadoDado.set_crs(epsg=4326)
+
+resultadoDado = resultadoDado.to_crs(epsg=3857)
+
+resultadoDado['centroid'] = resultadoDado.geometry.centroid
+
+resultadoDado = resultadoDado[resultadoDado['centroid'].notnull()]
+
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.grid(True, color='black', linestyle='--', linewidth=0.2)
+
+plt.title(f"Pontos de Desmatamento - Ano {anoProcurado}", fontsize=14)
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+
+resultadoDado['centroid'].apply(lambda x: plt.plot(x.x, x.y, marker='o', color='red', markersize=1))
+
+plt.show()
 
 tempoFinal = time.time()
 tempoTotal = round(tempoFinal - tempoInicio)
